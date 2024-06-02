cc48db55ba496caebb12934f64711bcf849960e6
alanq549 <40131239+alanq549@users.noreply.github.com>
2024-06-01 21:35:44 -0600

    Add files via upload

---
+++ js/sidebar.js
@@ -0,0 +1,25 @@
+// Obtener referencias a los elementos HTML
+const sidebar = document.getElementById('sidebar');
+const toggleButton = document.getElementById('ocultar');
+const mostrarButton = document.getElementById('mostrar');
+const contenido = document.getElementById('content');
+
+// Agregar evento de clic al botón "☰/ocultar"             
+toggleButton.addEventListener('click', () => {
+  sidebar.classList.add('d-none');
+  toggleButton.style.display = 'none';
+  mostrarButton.style.display = 'block';
+  // Modificar las clases de contenido al ocultar la barra lateral
+  contenido.classList.remove('col-md-10', 'col-sm-9');
+  contenido.classList.add('col-md-15', 'col-sm-13');
+
+});
+
+// Agregar evento de clic al botón "mostrar"
+mostrarButton.addEventListener('click', () => {
+  sidebar.classList.remove('d-none');
+  toggleButton.style.display = 'block';
+  mostrarButton.style.display = 'none';
+  // Modificar las clases de contenid5o al mostrar la barra lateral
+  contenido.classList.remove('col-md-15', 'col-sm-9');
+  contenido.classList.add('col-md-10', 'col-sm-9');});
